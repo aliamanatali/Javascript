5348eb826f36a2b4b27c6c34924960ed269f09f5
Ali Amanat Ali <aliamanatali218@gmail.com>
2023-03-05 11:53:19 +0500

    Learning Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+let js="amazing";
+console.log(40+8+23-10);
+console.log("jonas");
+console.log(23);
+
+let firstName = "Matlida";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
